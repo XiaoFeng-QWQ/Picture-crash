d6d4d45ecb73f0899d8ef75cc3dbdf83226cf9e6
XiaoFeng-QWQ <q1432777209@126.com>
2024-05-26 01:08:36 +0800

    崩坏逻辑优化

--- js/main.js
+++ js/main.js
@@ -2,10 +2,7 @@ const fileInput = document.getElementById('fileInput');
 const base64Result = document.getElementById('base64Result');
 const imageContainer = document.getElementById('imageContainer');
 const settingsContainer = document.getElementById('corruptImageSettings');
-// 监听转换按钮点击事件
-document.getElementById('convertButton').addEventListener('click', convertToBase64);
-// 监听损坏按钮点击事件
-document.getElementById('corruptButton').addEventListener('click', corruptImage);
+
 /**
  * 图片转换为Base64
  */
@@ -14,14 +11,16 @@ function convertToBase64() {
     if (file) {
         const reader = new FileReader();
         reader.onload = function (e) {
-            base64Result.value = e.target.result;
-            displayImage(e.target.result);
+            const base64Data = e.target.result;
+            base64Result.value = base64Data;
+            displayImage(base64Data);
         };
         reader.readAsDataURL(file);
     } else {
         alert('请选择要转换的图片！');
     }
 }
+
 /**
  * 显示图片
  * @param {string} base64Data - 图片的Base64数据
@@ -29,9 +28,17 @@ function convertToBase64() {
 function displayImage(base64Data) {
     const img = document.createElement('img');
     img.src = base64Data;
-    imageContainer.innerHTML = '';
+    clearImageContainer();
     imageContainer.appendChild(img);
 }
+
+/**
+ * 清空图片容器
+ */
+function clearImageContainer() {
+    imageContainer.innerHTML = '';
+}
+
 /**
  * 图片崩坏
  */
@@ -39,25 +46,40 @@ function corruptImage() {
     const img = imageContainer.querySelector('img');
     if (img) {
         let base64Data = img.src.split(',')[1];
+        const numModifications = parseInt(settingsContainer.querySelector('#numModifications').value);
         const dataLength = base64Data.length;
-        const startIndex = Math.floor(Math.random() * (dataLength - settingsContainer.querySelector('#startIndex').value));
-        const offset = Math.floor(Math.random() * settingsContainer.querySelector('#offset').value) + 3;
-        const charToModify = base64Data.charAt(startIndex);
-        const modifiedChar = String.fromCharCode(charToModify.charCodeAt(0) + offset);
-        base64Data = base64Data.substring(0, startIndex) +
-            modifiedChar +
-            base64Data.substring(startIndex + 1);
+
+        // 创建一个数组，存储要修改的位置
+        const modificationIndices = [];
+        for (let i = 0; i < numModifications; i++) {
+            modificationIndices.push(Math.floor(Math.random() * dataLength));
+        }
+
+        // 修改Base64数据
+        const corruptedBase64Data = base64Data.split('');
+        modificationIndices.forEach(index => {
+            const offset = Math.floor(Math.random() * parseInt(settingsContainer.querySelector('#offset').value));
+            const charCode = corruptedBase64Data[index].charCodeAt(0) + offset;
+            corruptedBase64Data[index] = String.fromCharCode(charCode);
+        });
+
+        // 生成损坏图片
         const mimeType = img.src.split(';')[0].split(':')[1];
-        const corruptedImg = new Image();
-        corruptedImg.onload = function () {
-            URL.revokeObjectURL(corruptedImg.src);
-        };
-        corruptedImg.src = `data:${mimeType};base64,${base64Data}`;
-        corruptedImg.alt = '图片损坏了！';
-        imageContainer.innerHTML = '';
-        imageContainer.appendChild(corruptedImg);
-        base64Result.value = corruptedImg.src;
+        const corruptedImgSrc = `data:${mimeType};base64,${corruptedBase64Data.join('')}`;
+        displayImage(corruptedImgSrc);
+        base64Result.value = corruptedImgSrc;
     } else {
         alert('请先选择图片并转换为Base64！');
     }
+}
+
+/**
+ * 替换字符串中指定位置的字符
+ * @param {string} str - 要修改的字符串
+ * @param {number} index - 要修改的位置
+ * @param {string} char - 修改后的字符
+ * @returns {string} - 修改后的字符串
+ */
+function replaceCharAt(str, index, char) {
+    return str.substring(0, index) + char + str.substring(index + 1);
 }
